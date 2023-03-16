ec814ca9bd43eca1f80a975ac6e571d4def9f868
Abdul <defvzo@gmail.com>
2023-03-16 09:22:02 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - c program that prints aline with puts function
+ * return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
